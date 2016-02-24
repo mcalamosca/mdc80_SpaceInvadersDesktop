fc979a5e7d4797f5babf90fdbabb7d9f9c5bfa22
mcalamosca <mcalamosca@gmail.com>
2016-02-23 22:50:55 -0500

    more changes to scoretracker

--- src/edu/pitt/is1017/spaceinvaders/ScoreTracker.java
+++ src/edu/pitt/is1017/spaceinvaders/ScoreTracker.java
@@ -6,70 +6,107 @@ import java.util.UUID;
 import javax.swing.JOptionPane;
 
 public class ScoreTracker {
-		
-	
+
+
 	private User user;
-	private int currentScore,highestScore;
-	private String gameID;
-	
+	private int currentScore,highestScore,highestScoreOther;
+	private String gameID,firstNameOther,lastNameOther;
+	;
+
 	public ScoreTracker(User user){
 		this.user=user;
 		this.currentScore=0;
 		this.gameID = UUID.randomUUID().toString();
-		
+
 		DbUtilities db = new DbUtilities();
-		
+
 		//Initialize row in db for current game
 		String initScore = "INSERT INTO alieninvasion.runningscores(`gameID`,`scoreType`,`scoreValue`,`fk_userID`,`dateTimeEntered`) ";
 		initScore += "VALUES (\""+this.gameID+"\","+1+",\"" +this.currentScore+"\",\""+this.user.getUserID()+"\",NOW());";
-		
+
 		db.executeQuery(initScore);
-		
+
 		//Select highest score for user
 		String highScore = "SELECT MAX(scoreValue) FROM alieninvasion.finalscores ";
 		highScore	+= "WHERE `fk_userID` = \"" + user.getUserID() + "\"";
-			
+
 		ResultSet rs = db.getResultSet(highScore);
-		
+
 		try {
-			highestScore = rs.getInt("scoreValue");
+			while(rs.next())
+				highestScore = rs.getInt("MAX(scoreValue)");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		db.closeConnection();
 	}
-	
+
 	public void recordScore(int point){
 		DbUtilities db = new DbUtilities();		
-		
+
 		if(point == 1){
 			currentScore++;
 		}
 		else if(point == -1){
 			currentScore--;
 		}
-		
+
 		//updates the score for the current game 
 		String updateScore = "UPDATE alieninvasion.runningscores SET `scoreValue`="+ getCurrentScore() +" ";
 		updateScore += "WHERE `gameID`=\""+this.gameID+"\" AND `fk_userID`="+this.user.getUserID()+";";
-		
+
 		db.executeQuery(updateScore);
 		db.closeConnection();
 	}
-	
+
 	public void recordFinalScore(){
 		DbUtilities db = new DbUtilities();		
-
+		
+		//Insert final score of current game
 		String sql = "INSERT INTO alieninvasion.finalscores(`gameID`,`scoreValue`,`fk_userID`,`dateTimeEntered`) ";
 		sql += "VALUES (\""+this.gameID+"\",\"" +this.currentScore+"\",\""+this.user.getUserID()+"\",NOW());";
-		
+
 		db.executeQuery(sql);
+		
+		//Retrieve highest score of user
+		String highScore = "SELECT MAX(scoreValue) FROM alieninvasion.finalscores ";
+		highScore	+= "WHERE `fk_userID` = \"" + user.getUserID() + "\"";
+
+		ResultSet rs = db.getResultSet(highScore);
+
+		try {
+			while(rs.next())
+				highestScore = rs.getInt("MAX(scoreValue)");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//Retrieve highest score on table, and display it
+		
+		String highScoreOther = "SELECT lastName, firstName, MAX(scoreValue) ";
+		highScoreOther += "FROM finalscores JOIN users ON fk_userID = userID GROUP BY lastName, firstName ORDER BY `MAX(scoreValue)` DESC";
+
+		ResultSet rs2 = db.getResultSet(highScoreOther);
+
+		try {
+			if(rs2.next()){
+				highestScoreOther = rs2.getInt("MAX(scoreValue)");
+				firstNameOther = rs2.getString("firstName");
+				lastNameOther = rs2.getString("lastName");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		db.closeConnection();
-		
-		JOptionPane.showMessageDialog(null, "Congratulations! You've won. \nYour score was: " + currentScore + "\nYour Highest Score was: " + highestScore);
-		
+
+		JOptionPane.showMessageDialog(null, "Congratulations! You've won. \nYour score was: " + currentScore + "\nYour Highest Score: "
+				+ highestScore + "\n\nOverall High Score\n"+firstNameOther+" "+lastNameOther+ ": "+highestScoreOther);
+
 	}
 
 	public int getCurrentScore() {
@@ -79,6 +116,6 @@ public class ScoreTracker {
 	public int getHighestScore() {
 		return highestScore;
 	}
-	
-	
+
+
 }
